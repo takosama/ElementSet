6ccd0bc5f0d43f52f0947b7faea1b9c672d609a4
takosama <takosama.312@gmail.com>
2017-12-16 22:53:16 +0900

    Update ElementSet.cs

--- ElementSet.cs
+++ ElementSet.cs
@@ -1,10 +1,11 @@
+ //this algorithm think by claclator
     class ElementSet<T> where T : IEquatable<T>
     {
         public IEnumerable<T> nums;
-        public Element(IEnumerable<T> nums) => this.nums = nums;
-        public Element(Element<T> e) => this.nums = e.nums;
+        public ElementSet(IEnumerable<T> nums) => this.nums = nums;
+        public ElementSet(ElementSet<T> e) => this.nums = e.nums;
 
-        public static Element<T> operator -(Element<T> a, Element<T>  b) 
+        public static ElementSet<T> operator -(ElementSet<T> a, ElementSet<T>  b) 
         {
             List<T> rtn = new List<T>();
             foreach (var num in a.nums)
@@ -12,18 +13,18 @@
                 if (b.nums.All(x => !x.Equals(num)))
                     rtn.Add(num);
             }
-            return new Element<T>(rtn);  
+            return new ElementSet<T>(rtn);  
         }
 
-        public static Element<T> operator +(Element<T> a, Element<T> b)
+        public static ElementSet<T> operator +(ElementSet<T> a, ElementSet<T> b)
         {
             var rtn = new List<T>();
             rtn.AddRange(a.nums);
             rtn.AddRange(b.nums);
-            return new Element<T>(rtn.Distinct().ToArray());
+            return new ElementSet<T>(rtn.Distinct().ToArray());
         }
 
-        public static Element<T> operator *(Element<T> a, Element<T> b)
+        public static ElementSet<T> operator *(ElementSet<T> a, ElementSet<T> b)
         {
             var rtn = new List<T>();
             foreach (var num in a.nums)
@@ -31,6 +32,6 @@
                 if (b.nums.Contains(num))
                     rtn.Add(num);
             }
-            return new Element<T>(rtn.ToArray());
+            return new ElementSet<T>(rtn.ToArray());
         }
     }
